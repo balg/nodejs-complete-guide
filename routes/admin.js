27548ea4507b60d139a9345022eb0c30829a75b0
balg <balazs@trekhunt.com>
2020-01-21 18:04:48 +0100

    Serves files instead of dummy html

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router(); // Router is like an app inside of the express app. It has the `use`, `get`, `post`, ... methods.
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.send(`
-    <form action="/admin/add-product" method="POST">
-      <input type="text" name="title" />
-      <button type="submit">Add Product</button>
-    </form>`);
+router.get('/add-product', (req, res) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
