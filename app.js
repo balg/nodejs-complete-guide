4854bcded9151efdd5ecfaa6b761c0dcc9643b11
balg <balazs@trekhunt.com>
2020-01-21 16:33:53 +0100

    Using body-parser + req filtering

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Add Product</h1>');
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" />
+      <button type="submit">Add Product</button>
+    </form>`);
+});
+
+// Same syntax as `use`. Also the same functionality it's just filterd to POST requests.
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
