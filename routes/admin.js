af5fdb8d7bd04d9234808ff76f04f02322bad4d9
balg <balazs@trekhunt.com>
2020-01-21 18:14:09 +0100

    util method to get rootDir

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router(); // Router is like an app inside of the express app. It has the `use`, `get`, `post`, ... methods.
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
