bd0a236d5bc53eacb57fb0e82c004c2d5b9c05ff
balg <balazs@trekhunt.com>
2020-01-21 15:42:19 +0100

    basic middlewares

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
-const routes = require('./routes');
-console.log(routes.someText);
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
