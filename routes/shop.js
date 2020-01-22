f4b45dfa70c563a6a391c41bfb1558b175342d59
balg <balazs@trekhunt.com>
2020-01-22 14:32:49 +0100

    Dynamic content with pug

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,11 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('shop');
+  const { products } = adminData;
+  res.render('shop', {
+    products,
+    docTitle: 'Shop'
+  });
 });
 
 module.exports = router;
